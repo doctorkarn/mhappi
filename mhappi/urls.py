648f459b2e4ae08b75ac035d2089f0974ff72313
Doctorkarn Methaphan <dr-karn@hotmail.com>
2015-09-23 15:47:34 +0700

    Change folder structure

--- mhappi/urls.py
+++ mhappi/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from apps.medical import views as med_view
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^medical/', med_view.index, name='index'),
+	
 ]
